3195b8ea33b9e0bcc9477c28dc233532fa4279e0
CainLuo <350116542@qq.com>
2018-05-29 17:32:06 +0800

    添加CLAMap2DExmaple工程

---
+++ CLAMap2DExmaple/CLAMap2DExmaple/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CLAMap2DExmaple
+//
+//  Created by Cain Luo on 2018/5/29.
+//  Copyright © 2018年 Cain Luo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
